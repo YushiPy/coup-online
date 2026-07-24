aa7a189fd16965a5e8ca7703dcde234a01d193cd
pedrobgennari <pedro.pbg@usp.br>
2026-07-24 13:58:28 -0300

    fix(backend::tests): remove duplicate test files

--- src/backend/tests/test_engine/test_player.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from backend.engine.player import Player
-
-def test_init():
-    # Tests player1 initialization
-    player1 = Player("ABCXYZ", "João", 2)
-    assert(repr(player1) == "ID: ABCXYZ | Name: João | Coins: 2 \n| Alive: True | Cards: []")
